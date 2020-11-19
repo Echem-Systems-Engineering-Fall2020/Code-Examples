8d16e8e40d79d4635c670c544b884099afeac067
decaluwe <steven.decaluwe@gmail.com>
2020-11-19 15:53:11 -0700

    Adding gas phase variables.
    - Adding C_k in anode catalyt layer to SV.
    - Created ptr class for locations within SV.
    - Added C_k_gas_CL balance equations
    - Still need to enable needed parameters.

--- pemfc/pemfc_function.py
+++ pemfc/pemfc_function.py
@@ -6,7 +6,7 @@
             dSV_dt: time derivative of solution vector variables SV.
 """
 import numpy as np
-from pemfc_init import pars
+from pemfc_init import pars, ptr
 from math import exp
 
 # Constants:
@@ -14,19 +14,48 @@ F = 96485    # Faraday's constant, C/mol of equivalent
 R = 8.3145   # Universal gas constant, J/mol-K
 
 def residual(t,SV):
-    dSV_dt = np.zeros_like(SV)
     
-    eta_an = SV[0] - pars.delta_Phi_eq_an
+    # Initialize the residual: must be the exact same size as SV.  Initialize 
+    #   as all zeros; that way, a variable will not change with time, if we do 
+    #   not specify a residual.
+    dSV_dt = np.zeros_like(SV)
+
+    "========ANODE==========="
+    # Calculate overpotential.
+    # SHOULD BE A FUNCTION OF SPECIES ACTIVITIES:
+    eta_an = SV[ptr.phi_dl_an] - pars.delta_Phi_eq_an
+
+    # Butler-Vollmer equation:
     i_Far_an = pars.i_o_an*(exp(-pars.n_an*F*pars.beta_an*eta_an/R/pars.T)
                       - exp(pars.n_an*F*(1-pars.beta_an)*eta_an/R/pars.T))
+
+    # Double layer current density (per unit area surface)
     i_dl_an = pars.i_ext - i_Far_an
-    dSV_dt[0] = -i_dl_an/pars.C_dl_an
+
+    # Change in double layer potential per time:
+    dSV_dt[ptr.phi_dl_an] = -i_dl_an/pars.C_dl_an
+
+    # Catalyst layer gas phase
+    " THESE ARE TEMPORARY "
+    C_k_an_CL = SV[ptr.C_k_an_CL]
+    N_k_i = np.zeros_like(C_k_an_CL)
+    sdot_k = np.zeros_like(N_k_i)
+    pars.A_fac = 1.
+    pars.eps_dy_Inv_CL = 1.
     
+    # Change in catalyst layer gas phase mole fractions:
+    dCk_dt = (N_k_i + sdot_k/pars.A_fac)*pars.eps_dy_Inv_CL
+    dSV_dt[ptr.C_k_an_CL] = dCk_dt
     
-    eta_ca = SV[1] - pars.delta_Phi_eq_ca
+
+
+
+
+    "========CATHODE==========="
+    eta_ca = SV[3] - pars.delta_Phi_eq_ca
     i_Far_ca = pars.i_o_ca*(exp(-pars.n_ca*F*pars.beta_ca*eta_ca/R/pars.T)
                       - exp(pars.n_ca*F*(1-pars.beta_ca)*eta_ca/R/pars.T))
     i_dl_ca = pars.i_ext - i_Far_ca
     
-    dSV_dt[1] = -i_dl_ca/pars.C_dl_ca
+    dSV_dt[3] = -i_dl_ca/pars.C_dl_ca
     return dSV_dt
--- pemfc/pemfc_init.py
+++ pemfc/pemfc_init.py
@@ -4,6 +4,7 @@
     
     Read in user inputs, then construct initial solution vector SV_0.  Create a class for all other needed parameters, called 'params.'
 """
+import numpy as np
 
 """ BEGIN USER INPUTS """
 
@@ -12,7 +13,10 @@ i_ext = 20
 t_final = 100
 
 T = 298   # Simulation temperature, K
+P_an_0 = 101325 #Pa
+X_k_an_0 = np.array([0.97, 0.03])
 
+" Electric potentials"
 phi_an_0 = 0
 phi_elyte_0 = 0.6
 phi_ca_0 = 1.1
@@ -24,18 +28,25 @@ i_o_an = 2.5
 i_o_ca = 1
 n_an = 2
 n_ca = 4
+
 F = 96485
+R = 8.3145
+
 beta_ca = 0.5
 beta_an = 0.5
 
 delta_Phi_eq_an = 0.61
 delta_Phi_eq_ca = 0.55
 """ END USER INPUTS """
-# Import necessary modules:
-import numpy as np 
+
+
+"============ INITIALIZE SOLUTION VECTOR ============"
+C_k_an_CL_0 = P_an_0*X_k_an_0/R/T
+
 
 # Construct initial solution vector
-SV_0 = np.array([phi_elyte_0 - phi_an_0, phi_ca_0 - phi_elyte_0])
+SV_0 = np.hstack((np.array([phi_elyte_0 - phi_an_0]), C_k_an_CL_0, 
+    np.array([phi_ca_0 - phi_elyte_0])))
 
 # Construct class containing all parameters.  Mostly we are just copying the 
 #   variable names from the user inputs into the class structure:
@@ -62,3 +73,13 @@ class pars:
 
     C_dl_ca = C_dl_ca
 
+# Create a 'pointer' class that specifies where in SV certain variables are 
+#    stored:
+class ptr:
+    phi_dl_an = 0
+
+    # C_k in anode CL: starts just after phi_dl, is same size as X_k_an:
+    C_k_an_CL = np.array([phi_dl_an+1, phi_dl_an+X_k_an_0.shape[0]])
+
+    # phi_dl_ca: starts just after C_k_an_CL:
+    phi_dl_ca = C_k_an_CL[-1] + 1
