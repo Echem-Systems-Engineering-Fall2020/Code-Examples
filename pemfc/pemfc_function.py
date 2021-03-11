ef7c0eee3c356eb76577a34be8de8b9ad082fe0c
decaluwe <steven.decaluwe@gmail.com>
2021-03-11 10:02:05 -0700

    Adding functions to modularize derivative calc.

--- pemfc/pemfc_function.py
+++ pemfc/pemfc_function.py
@@ -19,54 +19,39 @@ def residual(t, SV, pars, ptr):
     dSV_dt = np.zeros_like(SV)
 
     # GDL gas phase:
-    C_k_an_GDL = SV[ptr.C_k_an_GDL]
-    # Load the GDL state as s1
-    s1 = {'C_k': C_k_an_GDL, 'dy':pars.dy_GDL, 'eps_g':pars.eps_g_GDL, 
-        'n_Brugg':pars.n_Brugg_GDL, 'd_solid':pars.d_solid_GDL}
+    j = 0
+    s1, _  = read_state(SV, ptr, pars, 'gdl_an', j)
     
-    # First CL node gas phase:
-    C_k_an_2 = SV[ptr.C_k_an_CL[0,:]]
-    # Load this as state s2:
-    s2 = {'C_k': C_k_an_2, 'dy':pars.dy_CL, 'eps_g':pars.eps_g_CL,
-        'n_Brugg':pars.n_Brugg_CL, 'd_solid':pars.d_solid_CL}
+    # Read the state in the first CL node, save as state s2:
+    s2, eta_2 = read_state(SV, ptr, pars, 'cl_an', j)
 
     gas_props = {'T':pars.T, 'D_k':pars.D_k_g_an, 'mu':pars.mu_g_an}
 
     # Calculate the flux between states 1 (GDL) and 2 (first CL node)
     #   The subscript 'o' refers to the flux 'out' of the s2 node.
     N_k_o = pemfc_gas_flux(s1, s2, gas_props)
-    "========ANODE==========="
-    for j in np.arange(pars.npoints_CL_an-1):
+    "========ANODE CATALYST LAYER==========="
+    for j in np.arange(pars.npoints_CL-1):
         # Re-assign state 2 's2' as 's1', previously 'out' flux as 'in' flux.
         s1 = s2
         N_k_i = N_k_o
-
-        # Calculate overpotential.
-        # SHOULD BE A FUNCTION OF SPECIES ACTIVITIES:
-        eta_an = SV[ptr.phi_dl_an[j]] - pars.delta_Phi_eq_an 
+        eta = eta_2
     
-        # Butler-Vollmer equation:
-        # i_o SHOULD VARY WITH SPECIES ACTIVITIES:
-        i_Far_an = -pars.i_o_an*(exp(-pars.n_an*F*pars.beta_an*eta_an/R/pars.T)
-                        - exp(pars.n_an*F*(1-pars.beta_an)*eta_an/R/pars.T))
-
-        # Double layer current density (per unit area surface)
-        i_dl_an = -pars.i_ext*pars.A_fac_dl - i_Far_an*pars.f_Pt
-
+        # Calculate currents and chemical production rates:
+        i_dl, sdot_k = chem_calcs(eta, pars, 'an')
+       
         # Change in double layer potential per time:
-        dSV_dt[ptr.phi_dl_an[j]] = -i_dl_an/pars.C_dl_an
+        dSV_dt[ptr.phi_dl_an[j]] = -i_dl/pars.C_dl_an
 
         gas_props = {'T':pars.T, 'D_k':pars.D_k_g_an, 'mu':pars.mu_g_an}
 
         # 'Next' node gas phase
-        C_k_an_2 = SV[ptr.C_k_an_CL[j+1,:]]
-        s2= {'C_k': C_k_an_2, 'dy':pars.dy_CL, 'eps_g':pars.eps_g_CL,
-            'n_Brugg':pars.n_Brugg_CL, 'd_solid':pars.d_solid_CL}
+
+        # Read the state in the first CL node, save as state s2:
+        s2, eta_2 = read_state(SV, ptr, pars, 'cl_an', j+1)
         
         # Calculate fluxes between states 1 and 2:
         N_k_o = pemfc_gas_flux(s1, s2, gas_props)
-        # Molar production rates due to Faradaic current:
-        sdot_k = i_Far_an*pars.nu_k_an/pars.n_an/F
         
         # Change in catalyst layer gas phase mole fractions:
         dCk_dt = (N_k_i - N_k_o + sdot_k*pars.A_fac_Pt)*pars.eps_g_dy_Inv_CL
@@ -75,42 +60,78 @@ def residual(t, SV, pars, ptr):
     # Re-assign state 2 's2' as 's1'
     s1 = s2
     N_k_i = N_k_o
+    eta = eta_2
 
     # Look at the final CL node:
-    j = pars.npoints_CL_an-1
+    j = pars.npoints_CL-1
 
-    # Calculate overpotential.
-    # SHOULD BE A FUNCTION OF SPECIES ACTIVITIES:
-    eta_an = SV[ptr.phi_dl_an[j]] - pars.delta_Phi_eq_an 
-
-    # Butler-Vollmer equation:
-    # i_o SHOULD VARY WITH SPECIES ACTIVITIES:
-    i_Far_an = -pars.i_o_an*(exp(-pars.n_an*F*pars.beta_an*eta_an/R/pars.T)
-                    - exp(pars.n_an*F*(1-pars.beta_an)*eta_an/R/pars.T))
-
-    # Double layer current density (per unit area surface)
-    i_dl_an = -pars.i_ext*pars.A_fac_dl - i_Far_an*pars.f_Pt
+    # Calculate currents and chemical production rates:
+    i_dl, sdot_k = chem_calcs(eta, pars, 'an')
 
     # Change in double layer potential per time:
-    dSV_dt[ptr.phi_dl_an[j]] = -i_dl_an/pars.C_dl_an
-        
-    # Molar production rates due to Faradaic current:
-    sdot_k = i_Far_an*pars.nu_k_an/pars.n_an/F
+    dSV_dt[ptr.phi_dl_an[j]] = -i_dl/pars.C_dl_an
     
     # Change in catalyst layer gas phase mole fractions:
     dCk_dt = (N_k_i - N_k_o + sdot_k*pars.A_fac_Pt)*pars.eps_g_dy_Inv_CL
     dSV_dt[ptr.C_k_an_CL[j,:]] = dCk_dt
 
-
     "========CATHODE==========="
     " THIS SHOULD EVENTUALLY DEPEND ON SPECIES ACTIVITIES. "
-    eta_ca = SV[ptr.phi_dl_ca] - pars.delta_Phi_eq_ca
-    i_Far_ca = pars.i_o_ca*(exp(-pars.n_ca*F*pars.beta_ca*eta_ca/R/pars.T)
-                      - exp(pars.n_ca*F*(1-pars.beta_ca)*eta_ca/R/pars.T))
-    i_dl_ca = pars.i_ext*pars.A_fac_dl - i_Far_ca*pars.f_Pt
-    dSV_dt[ptr.phi_dl_ca] = -i_dl_ca/pars.C_dl_ca
+    eta = SV[ptr.phi_dl_ca] - pars.delta_Phi_eq_ca
+    
+    i_dl, _ = chem_calcs(eta, pars, 'ca')
+
+    dSV_dt[ptr.phi_dl_ca] = -i_dl/pars.C_dl_ca
+
+    # Return the SV derivatives w/r/t time:
     return dSV_dt
 
+def read_state(SV, ptr, pars, domain, j):
+    if domain=='gdl_an':
+        # No ionomer, so no eta:
+        eta = 0
+
+        # Read out gas phase molar concentrations:
+        C_k = SV[ptr.C_k_an_GDL[j]]
+        # Load the GDL state as s1
+        state = {'C_k': C_k, 'dy':pars.dy_GDL, 'eps_g':pars.eps_g_GDL, 
+            'n_Brugg':pars.n_Brugg_GDL, 'd_solid':pars.d_solid_GDL}
+        
+    elif domain=='cl_an':
+        # Calculate overpotential.
+        # SHOULD BE A FUNCTION OF SPECIES ACTIVITIES:
+        eta = SV[ptr.phi_dl_an[j]] - pars.delta_Phi_eq_an 
+
+        # Read out gas phase molar concentrations:
+        C_k = SV[ptr.C_k_an_CL[j]]
+        # Load the GDL state as s1
+        state = {'C_k': C_k, 'dy':pars.dy_CL, 'eps_g':pars.eps_g_CL, 
+            'n_Brugg':pars.n_Brugg_CL, 'd_solid':pars.d_solid_CL}
+
+    return state, eta
+
+def chem_calcs(eta, pars, domain):
+    if domain == 'an':
+        # Butler-Vollmer equation:
+        # i_o SHOULD VARY WITH SPECIES ACTIVITIES:
+        i_Far = -pars.i_o_an*(exp(-pars.n_an*F*pars.beta_an*eta/R/pars.T)
+                        - exp(pars.n_an*F*(1-pars.beta_an)*eta/R/pars.T))
+
+        # Molar production rates due to Faradaic current:
+        sdot_k = i_Far*pars.nu_k_an/pars.n_an/F
+
+        # Double layer current density (per unit area surface)
+        i_dl = -pars.i_ext*pars.A_fac_dl - i_Far*pars.f_Pt
+    elif domain=='ca':
+        i_Far = pars.i_o_ca*(exp(-pars.n_ca*F*pars.beta_ca*eta/R/pars.T)
+                          - exp(pars.n_ca*F*(1-pars.beta_ca)*eta/R/pars.T))
+        # Molar production rates due to Faradaic current:
+        sdot_k = i_Far*pars.nu_k_ca/pars.n_ca/F
+
+        # Double layer current density (per unit area surface)
+        i_dl = pars.i_ext*pars.A_fac_dl - i_Far*pars.f_Pt
+
+    return i_dl, sdot_k
 
 def pemfc_gas_flux(node1, node2, gas_props):
     # Initialize molar fluxes:
--- pemfc/pemfc_init.py
+++ pemfc/pemfc_init.py
@@ -1,75 +1,14 @@
 """ Initialize PEMFC model.
 
-    For now, we hard-code all inputs.  Eventually, we may move them to a 'pemfc_inputs.py' file.
-    
-    Read in user inputs, then construct initial solution vector SV_0.  Create a class for all other needed parameters, called 'params.'
+    1. Read in user inputs.
+    2. Construct initial solution vector SV_0.
+    3. Create a class for all other needed parameters, called 'pars.' 
+    4. Create a class containing all variable pointers, called 'ptr.'
 """
 import numpy as np
 
-""" BEGIN USER INPUTS """
-save_tag = 'porosity_study'
-i_ext = 10000  # External current (A/m2)
-
-t_final = 100000
-
-" Thermo-chemical inputs "
-T = 298   # Simulation temperature, K
-P_an_0 = 101325 #Pa
-X_k_an_0 = np.array([0.97, 0.03])
-
-" Microstructure and geometry "
-eps_gas_GDL = 0.7 # Gas phase volume fraction in GDL
-
-eps_solid_CL = 0.6 # Volume fraction of solids (Pt +C) in CL
-eps_gas_CL = 0.28  # Volume fraction of gas in CL
-
-# Exponent in the Bruggeman correlatin: tau_fac = eps^alpha
-alpha_Brugg_GDL = -1
-alpha_Brugg_CL = -0.5
-
-Pt_surf_frac = 0.1 # Fraction of carbon surface covered by Pt in CL.
-
-dy_GDL = 100e-6 # GDL thickness (m)
-dy_CL = 20e-6   # CL thickness (m)
-npoints_CL_an = 3  # Number of finite volumes/mesh points in anode CL
-
-# Carbon particle diameter:
-d_part_GDL = 5e-6 # m
-d_part_CL = 100e-9 # m
-
-" Transport properties"
-D_k_g_an = np.array([5.48e-4, 6.13e-5]) #Mixture-averaged diffusion coeffs, m2/s
-mu_g_an = 9.54e-6                       # Dynamic viscsity, Pa-s
-
-" Initial electric potential values "
-phi_an_0 = 0
-phi_elyte_0 = 0.6
-phi_ca_0 = 1.1
-
-" Charge transfer inputs "
-C_dl_an = 1e2 # F/m2
-C_dl_ca = 1e2 # F/m2
-
-i_o_an = 2.5e-1
-i_o_ca = 1e-1
-
-n_an = -2.
-n_ca = 4
-
-F = 96485
-R = 8.3145
-
-# Charge transfer stoichiometric coefficients:
-# Must be in same order as X_k_an:
-nu_k_an = np.array([-1., 0.])
-
-beta_ca = 0.5
-beta_an = 0.5
-
-delta_Phi_eq_an = -0.61
-delta_Phi_eq_ca = 0.55
-""" END USER INPUTS """
-
+"============ READ IN USER INPUTS ============"
+from pemfc_inputs import *
 
 "============ INITIALIZE SOLUTION VECTOR ============"
 C_k_an_0 = P_an_0*X_k_an_0/R/T
@@ -78,10 +17,11 @@ C_k_an_0 = P_an_0*X_k_an_0/R/T
 # Construct initial solution vector
 SV_0_GDL_an = C_k_an_0
 SV_0_CL_an = np.tile(np.hstack((np.array([phi_an_0 - phi_elyte_0]),C_k_an_0)),
-    npoints_CL_an)
+    npoints_CL)
 SV_0_ca = np.array([phi_ca_0 - phi_elyte_0])
 SV_0 = np.hstack((SV_0_GDL_an, SV_0_CL_an, SV_0_ca))
 
+"============ CREATE PARAMETERS CLASS ============"
 # Construct class containing all parameters.  Mostly we are just copying the 
 #   variable names from the user inputs into the class structure:
 class pars:
@@ -100,8 +40,8 @@ class pars:
     C_dl_an = C_dl_an
 
     dy_GDL = dy_GDL
-    dy_CL = dy_CL/npoints_CL_an
-    npoints_CL_an = npoints_CL_an
+    dy_CL = dy_CL/npoints_CL
+    npoints_CL = npoints_CL
 
     eps_g_dy_Inv_CL = 1/dy_CL/eps_gas_CL
     eps_g_dy_Inv_GDL = 1/dy_GDL/eps_gas_GDL
@@ -112,6 +52,7 @@ class pars:
     n_Brugg_CL = alpha_Brugg_CL
 
     nu_k_an = nu_k_an
+    nu_k_ca = nu_k_ca
 
     X_k_GDL = X_k_an_0
 
@@ -137,6 +78,7 @@ class pars:
 
     C_dl_ca = C_dl_ca
 
+"============ CREATE CLASS OF POINTERS ============"
 # Create a 'pointer' class that specifies where in SV certain variables are 
 #    stored:
 class ptr:
@@ -145,11 +87,11 @@ class ptr:
     C_k_an_GDL = np.arange(0, X_k_an_0.shape[0])
     
     # C_k in anode CL: starts just after GDL, is same size as X_k_an:
-    phi_dl_an = np.empty((npoints_CL_an),int)
-    C_k_an_CL = np.empty((npoints_CL_an, X_k_an_0.shape[0]),int)
+    phi_dl_an = np.empty((npoints_CL),int)
+    C_k_an_CL = np.empty((npoints_CL, X_k_an_0.shape[0]),int)
     nvars_an_CL = 1+X_k_an_0.shape[0]
     
-    for j in np.arange(npoints_CL_an):
+    for j in np.arange(npoints_CL):
         phi_dl_an[j] = C_k_an_GDL[-1] + 1 + j*nvars_an_CL
         C_k_an_CL[j,:] = np.arange(phi_dl_an[j]+1, phi_dl_an[j]+1+X_k_an_0.shape[0])
     
